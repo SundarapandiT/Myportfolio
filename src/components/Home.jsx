ce1d3cd6073df2b3a25a01047383409a3765de40
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2025-10-13 19:23:59 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <section id="home">
     <div className='content'>
     <h2>Hello..!</h2>
-    <h1>I'm <b>SundaraPandi T</b></h1>
+    <h1>I'm <b>SundaraPandi</b></h1>
     <h3>(Computer Science Engineering Student)</h3>
     <button onClick={() => window.open('https://www.linkedin.com/in/sundarapandi-t-5a1772247', '_blank')}>Contact me</button>
     </div>
